91d5b146bcc5beabafd03037051559845e7d49e7
Philipp Hirsch <itself@hanspolo.net>
2016-09-28 11:47:22 +0200

    Get live data from facebook via graphs#new and graphs#create and display them on graphs#index

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,19 +1,34 @@
 class GraphsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @friends = load_friends
+    @data = load_data
+  end
+
+  def new
   end
 
   def create
+    if current_user.update(facebook_id: params[:uid], facebook_key: params[:token])
+      flash[:notice] = 'Requested user id successfully'
+      redirect_to graphs_path
+    else
+      flash[:alert] = 'Could not request user id'
+      render :new
+    end
   end
 
   private
 
-  def load_friends
+  def load_data
     return [] if current_user.facebook_id.nil?
-    http = Net::HTTP.new('elastic', 9200)
-    response = http.get("https://graph.facebook.com/v2.7/#{current_user.facebook_id}?fields=friends", document)
-    if response.code == '200'
-      JSON.parse(response.body)['friends']['data']
-    end
+
+    uri = URI("https://graph.facebook.com/v2.7/#{current_user.facebook_id}")
+    params = { fields: 'name,friends', access_token: current_user.facebook_key }
+    uri.query = URI.encode_www_form(params)
+
+    response = Net::HTTP.get(uri)
+    @response = response
+    JSON.parse(response)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  resources :graphs, only: [:index, :create]
+  resources :graphs, only: [:index, :new, :create]
 end
