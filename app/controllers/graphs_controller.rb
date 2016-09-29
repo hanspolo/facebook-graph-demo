222f4f4d29a19d76e1c2762b9ee12488e12f047f
Philipp Hirsch <itself@hanspolo.net>
2016-09-29 23:10:11 +0200

    Receive longlive token after receiving user token

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,14 +2,14 @@ class GraphsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @data = load_data
+    @data = load_data if current_user.facebook_key.present?
   end
 
   def new
   end
 
   def create
-    if current_user.update(facebook_id: params[:uid], facebook_key: params[:token])
+    if current_user.update(facebook_id: params[:uid], facebook_key: get_longlive_token(params[:token]))
       flash[:notice] = 'Requested user id successfully'
       redirect_to graphs_path
     else
@@ -20,6 +20,15 @@ class GraphsController < ApplicationController
 
   private
 
+  def get_longlive_token(token)
+    uri = URI('https://graph.facebook.com/oauth/access_token')
+    params = { grant_type: 'fb_exchange_token', client_id: Rails.application.secrets.facebook_app_id,
+               client_secret: Rails.application.secrets.facebook_app_secret, fb_exchange_token: token }
+    uri.query = URI.encode_www_form(params)
+    response = Net::HTTP.get(uri)
+    response.split('&').first.split('=').last
+  end
+
   def load_data
     return [] if current_user.facebook_id.nil?
 
@@ -28,7 +37,6 @@ class GraphsController < ApplicationController
     uri.query = URI.encode_www_form(params)
 
     response = Net::HTTP.get(uri)
-    @response = response
     JSON.parse(response)
   end
 end
